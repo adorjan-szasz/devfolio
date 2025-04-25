7977b40b9662cd9237d6b37168af46aac0fd5efd
adorjan-szasz <szszadorjn@yahoo.com>
2025-04-25 17:47:30 +0300

    Tailwind build setup.

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  purge: ['./*.html', './*.js'],
+  darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+  content: [],
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
